d119ebe6ed5ea9de146f3617cc3cc4162e4091af
Mahesh Gorai <maheshgorai202@gmail.com>
2026-01-15 21:47:33 +0530

    Update MYS.R

--- MYS.R
+++ MYS.R
@@ -3,6 +3,7 @@ library(data.table)
 library(tidyr)
 
 
+
 perv1 <- fread("D:/UDISE+/perv1.txt")
 
 perv1 <- perv1 %>%
@@ -208,6 +209,7 @@ person_table <- perv1 %>%
                                        "50-54","55-59","60-64","65-69","70-74","75-79","80-84","85+")))
 
 View(person_table)
+
 
 
 
