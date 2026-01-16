165c519d5b0e87a911bcdf539eafaafa2ca202a7
Mahesh Gorai <maheshgorai202@gmail.com>
2026-01-16 22:02:42 +0530

    Update state_wise_literacy.R

--- state_wise_literacy.R
+++ state_wise_literacy.R
@@ -9,6 +9,7 @@ df <- fread("D:/UDISE+/perv1.txt")
 
 
 
+
 df <- df %>%
   mutate(State = Column6, State = as.character(State))
 df <- df %>%
@@ -130,6 +131,7 @@ state_urban_table_7plus <- df %>%
   ) %>%
   arrange(desc(Literacy_Rate_urban_7plus))
 View(state_urban_table_7plus)
+
 
 
 
