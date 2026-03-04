6755d989c063d69a4990c5f5fa387fda5a50da43
Mahesh Gorai <maheshgorai202@gmail.com>
2026-03-04 20:29:38 +0530

    Update Out_of_sch_8_state_PLFS.R

--- Out_of_sch_8_state_PLFS.R
+++ Out_of_sch_8_state_PLFS.R
@@ -3,7 +3,7 @@ library(data.table)
 library(tidyr)
 
 
-df <- fread("D:/UDISE+/perv1.txt")
+df <- fread("D:/*****/perv1.txt")
 never_enrolled_df <- df %>%
   mutate(
     Gender = case_when(
@@ -92,6 +92,7 @@ never_enrolled_table_state <- never_enrolled_table_state %>%
 
 specific_states_table <- never_enrolled_table_state %>%
   filter(State %in% c("5", "9","22", "23", "24", "25", "27", "30"))
+
 
 
 
